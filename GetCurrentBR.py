16b7a355fa4d85f95680103e76e77dd626916504
Horayuu <horatayuuki@gmail.com>
2025-12-20 23:49:55 +0900

    ver1.4.2

--- GetCurrentBR.py
+++ GetCurrentBR.py
@@ -1,13 +1,15 @@
 import datetime,re
-
-now = datetime.datetime.now()
-with open("/home/py/DiscordBot_Server/br_cache") as f:
-        cached_lines = f.readlines()
-
-
 DATE_PATTERN = re.compile(r"(\d{1,2}/\d{1,2}) ~ (\d{1,2}/\d{1,2})\s+(BR\s+\d{1,2}\.\d{1,1})")
-current_year = now.year
+
 def get_current_br():
+  now = datetime.datetime.now()
+  current_year = now.year
+  try:
+      with open("/home/py/DiscordBot_Server/br_cache") as f:
+        cached_lines = f.readlines()
+  except FileNotFoundError:
+      return "Cache File Not Found"
+
   for line in cached_lines:
     match = DATE_PATTERN.search(line)
     if match:
@@ -17,7 +19,7 @@ def get_current_br():
         try:
              start_dt = datetime.datetime.strptime(f"{current_year}/{start_date_str}", "%Y/%m/%d")
              end_dt = datetime.datetime.strptime(f"{current_year}/{end_date_str}", "%Y/%m/%d")
-             end_dt = end_dt.replace(hour=23, minute=59, second=59)
+             end_dt = end_dt.replace(hour=22, minute=59, second=59)
              if start_dt <= now <= end_dt:
                     return br_value
         except ValueError as e:
